83179a02b7214dc54a5280f9bfef341f12ce3ed0
JoelMWatson <jwatson10@mail.greenriver.edu>
2017-01-27 10:11:55 -0800

    Separated navbar from index  and files. Put navbar into includes directory. Changed about.html to php file

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 	try {
 		$dbh = new PDO("mysql:host=$hostname;
 		dbname=logan_madeinkent", $username, $password);
-		//echo "Connected to database.";
+		// echo "Connected to database.";
 	}
 	catch (PDOException $e) {
 		echo $e->getMessage(); 
@@ -48,39 +48,9 @@
 </head>
 
 <body>
-    
 
-	<nav class="navbar navbar-default" id="nav_bar" role="navigation">
-	  <div class="container">
-		<div class="navbar-header">
-		  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-			<span class="sr-only">Toggle navigation</span>
-			<span class="icon-bar"></span>
-			<span class="icon-bar"></span>
-			<span class="icon-bar"></span>
-		  </button>
-			<a class="navbar-brand" href="#">
-				<img alt="Brand" src="images/kent-wa-logo.png">
-			</a>
-			<div class="navbar-intro">
-				<h3>Welcome to MadeInKent BETA</h3>
-				<p>See the interesting stuff made in the city of Kent, Washington</p>
-			</div>
-		</div>
-		<div id="navbar" class="navbar-collapse collapse ">
-		  <ul class="nav navbar-nav navbar-right navbar-collapse collapse pull-xs-right">
-			<li class="active"><a href="index.php">Home</a></li>
-			
-			<li><a href="about.html" target="_blank">About</a></li>
-			
-			<li id="actBtn" class="navbar-btn">
-				<a href="http://www.kentwa.gov/home/showdocument?id=4655" target="_blank" class="btn btn-default ">Add your business!</a>
-			</li>
-		  </ul>
-		</div><!--/.nav-collapse -->
-	  </div><!-- Container-->
-	</nav>          
-	
+	<?php include_once "includes/navbar.php" ?>
+
 	<div class="content-wrapper">
 		<div class="map-wrapper">
 			<!-- Div to Display the map-->
