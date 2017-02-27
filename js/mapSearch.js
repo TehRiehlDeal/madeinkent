16795bc4348c0e148209a20df11e58042962a53d
JoelMWatson <jwatson10@mail.greenriver.edu>
2017-02-26 17:30:44 -0800

    Markers and divs are now restored after each backspace from the search bar

--- js/mapSearch.js
+++ js/mapSearch.js
@@ -3,44 +3,66 @@
  */
 
 var searchBar = $("#search");
+var mementoStack = [];
+var matches = [];
 
 $(document).ready(function (){
-    searchBar.on("keyup", function(){
-        var searchResult = new RegExp('^'+searchBar.val()+'.*', 'i');
-        var matches = [];
-        var markersToRemove = [];
-        $.each(allTitles, function (title){
-            if (searchResult.test(allTitles[title])){
-                matches.push(title);
-            }
-        });
-
-        if (matches[0] == null){
-            searchBar.fadeIn().html('').css("border", "3px solid red");
-        }
-        else if (searchResult == "/^.*/i"){
-            searchBar.fadeIn().html('').css("border","0px solid red");
-            map.markers.items.forEach(function(marker){
-                $("#"+marker.id).show();
-            });
-            map.replaceBy(function(marker){
-                return marker.map == null
-            })
+    searchBar.on("keyup", function(e){
+        var searchResult = new RegExp('^' + searchBar.val() + '.*', 'i');
+        if (searchResult == "/^.*/i" && mementoStack[0] == null ) {
+            //do nothing
+            //this keeps it from refreshing when backspace is hit before typing.
         }
         else {
-            $.each(allTitles, function(title ){
-                if (matches.includes(title)){
-                    //do nothing
-                }
-                else{
-                    map.removeBy(function (marker){
-                        return marker.id == title;
+            //check for backspace, empty mementoStack, and nonempty searchbar
+            if (e.which == 8 && mementoStack[0] != null && searchResult != "/^.*/i") {
+                var previousState = mementoStack.pop()
+                for (var i = 0; i < previousState.length; i++) {
+                    map.replaceBy(function (marker) {
+                        return marker.id == previousState[i] && marker.map == null;
                     });
-                    $("#" + title).hide();
+                    $("#" + previousState[i]).show();
+                }
+            }
+            //push to mementoStack when last key != backspace
+            if (e.which != 8) {
+                mementoStack.push(matches);
+            }
+            matches = [];
+            $.each(allTitles, function (title) {
+                if (searchResult.test(allTitles[title])) {
+                    matches.push(title);
                 }
             });
 
-            searchBar.fadeIn().html('').css("border", "0px solid red");
+            if (matches[0] == null) {
+                searchBar.fadeIn().html('').css("border", "3px solid red");
+            }
+            else if (searchResult == "/^.*/i") {
+                mementoStack = [];
+                searchBar.fadeIn().html('').css("border", "0px solid red");
+                map.markers.items.forEach(function (marker) {
+                    $("#" + marker.id).show();
+                });
+                map.replaceBy(function (marker) {
+                    return marker.map == null
+                })
+            }
+            else {
+                $.each(allTitles, function (title) {
+                    if (matches.includes(title)) {
+                        //do nothing
+                    }
+                    else {
+                        map.removeBy(function (marker) {
+                            return marker.id == title;
+                        });
+                        $("#" + title).hide();
+                    }
+                });
+
+                searchBar.fadeIn().html('').css("border", "0px solid red");
+            }
         }
 
         bizList.scrollTop = 0;
